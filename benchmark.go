166d2e3d964ecc17359ca9985f1f83c105567418
na20215 <na20215@bristol.ac.uk>
2021-11-29 17:13:05 +0000

    change bench mark

--- benchmark.go
+++ benchmark.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"testing"
+	"uk.ac.bris.cs/gameoflife/gol"
 )
 
 // Benchmark applies the filter to the ship.png b.N times.
 // The time taken is carefully measured by go.
 // The b.N  repetition is needed because benchmark results are not always constant.
 
-func BenchmarkFilter(b *testing.B) {
-	// Disable all program output apart from benchmark results
-	os.Stdout = nil
+func Benchmark(b *testing.B) {
+	tests := []gol.Params{
+		{ImageWidth: 512, ImageHeight: 512},
+	}
+	for _, p := range tests {
+		for _, turns := range []int{1000} {
+			p.Turns = turns
 
-	// Use a for-loop to run 5 sub-benchmarks, with 1, 2, 4, 8 and 16 workers.
-	for threads := 1; threads <= 16; threads *= 2 {
-		b.Run(fmt.Sprintf("%d_workers", threads), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-
+			for threads := 1; threads <= 16; threads++ {
+				p.Threads = threads
+				testName := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+				b.Run(testName, func(b *testing.B) {
+					events := make(chan gol.Event)
+					go gol.Run(p, events, nil)
+					for event := range events {
+						switch e := event.(type) {
+						case gol.FinalTurnComplete:
+							fmt.Println(e)
+							break
+						}
+					}
+				})
 			}
-		})
+		}
+		// os.Exit(2)
 	}
 }
